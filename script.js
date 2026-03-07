df62b1ec71277b401cba1e930d0065a102570e09
jainadavis <Jainadavis1@outlook.com>
2026-03-06 23:56:16 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const PASSWORD = "dillybar";
 
-function checkPassword() {
+function checkPassword(){
 
 let input = document.getElementById("password").value;
 let error = document.getElementById("error");
 
-if (input === PASSWORD) {
+if(input === PASSWORD){
+
+/* mark site as unlocked */
+localStorage.setItem("unlocked","true");
+
+/* go to message page */
 window.location.href = "message.html";
+
 }
-else {
+else{
+
 error.textContent = "Wrong password ❤️";
+
 }
 
 }
